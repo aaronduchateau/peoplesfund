880128bb5d573697b1807bbb02e55cf695463e32
Aaron DuChateau <chateauconcept@gmail.com>
2022-10-14 10:24:35 +0800

    add pitch deck

--- containers/Marketplace/components/SlideMenu.tsx
+++ containers/Marketplace/components/SlideMenu.tsx
@@ -9,6 +9,7 @@ const SlideMenu = () => {
         <div id="mySidenav" className={'sidenav ' + isOpenClass} onClick={()=>toggleOpen(!isOpen)}>
             <a className="closebtn" >&times;</a>
             <Link href="/"><a href="#">Home</a></Link>
+            <Link href="/pitch"><a href="#">The Pitch Deck</a></Link>
             <Link href="/termsOfService"><a href="#">Terms of Service</a></Link>
             <a href="#">Services</a>
             <a href="#">Clients</a>
---
+++ pages/pitch.tsx
@@ -0,0 +1,39 @@
+import 'react-slideshow-image/dist/styles.css';
+import { Zoom } from "react-slideshow-image";
+
+
+const images = ['./slides/s1.png','./slides/s2.png','./slides/s3.png','./slides/s4.png','./slides/s5.png','./slides/s6.png','./slides/s7.png','./slides/s8.png','./slides/s9.png','./slides/s10.png','./slides/s11.png','./slides/s12.png'];
+
+  const zoomOutProperties = {
+    duration: 10000,
+    transitionDuration: 500,
+    infinite: true,
+    indicators: true,
+    scale: 0.4,
+    arrows: true
+  };
+
+import HeaderSecondaryContainer from "../containers/Marketplace/components/HeaderSecondaryContainer";
+import Footer from "../containers/Marketplace/components/Footer";
+
+const pitch = () => {
+
+
+    return (
+        <div className="flex flex-1 flex-col min-h-screen text-gray-500  bg-fmug">
+            <HeaderSecondaryContainer termsUrl="./pitch.png" />
+            <div className="bg-brt-green p-5 flex justify-center">
+                <div className="slide-container w-4/5">
+                    <Zoom {...zoomOutProperties}>
+                        {images.map((each, index) => (
+                        <img key={index} style={{ width: "100%" }} src={each} />
+                        ))}
+                    </Zoom>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default pitch;
