bbeed4c5cba95a86e9d3efb8fa3ec98b8025f08e
Aaron DuChateau <chateauconcept@gmail.com>
2023-10-23 18:18:26 -0600

    more typscritp stuff

--- containers/Marketplace/components/HeroSection.tsx
+++ containers/Marketplace/components/HeroSection.tsx
@@ -5,12 +5,6 @@ const HeroSection = () => {
   return (
     <div className="flex flex-1 flex-col items-center justify-center hero-image-drone">
       <div className="items-center justify-center">
-        {/*<MbText
-          className="text-3xl uppercase p-4 border-gray-100"
-        >
-          NFT Membership Selection
-        </MbText> <img src="logo-x.png" className="flex items-center float-left p-4" style={{width: '130px'}} /> */}
-
         <div className="pl-4 pr-4 grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 gap-4 my-12 text-white">
           <div className="text-xl text-white mt-1 font-bold">
             <img src="./survey.png" />
@@ -18,7 +12,7 @@ const HeroSection = () => {
           </div>
           <div className="" style={{textShadow: '3px 1px 19px black', letterSpacing: '.05rem'}}>
             <h1 style={{color: '#2ba95c'}}>The Problem:</h1>  
-          <span style={{fontSize: '14px'}}>> The mission statement seems simple:</span> <Divider sx={{mt: 1, mb: 1}}/>
+          <span style={{fontSize: '14px'}}>&gt; The mission statement seems simple:</span> <Divider sx={{mt: 1, mb: 1}}/>
           <span style={{fontSize: '14px', color: '#2ba95c'}}>
             &nbsp;1) Get paid to keep trees standing <br /> &nbsp;2) Offset Carbon emissions 
           </span> 
@@ -27,7 +21,7 @@ const HeroSection = () => {
           </div>
           <div className="" style={{textShadow: '3px 1px 19px black', letterSpacing: '.05rem'}}>
             <h1 style={{color: '#2ba95c'}}>Our Solution:</h1>  
-            <span style={{fontSize: '14px'}}>> Technology to the Rescue: <Divider sx={{mt: 1, mb: 1}}/>
+            <span style={{fontSize: '14px'}}>&gt; Technology to the Rescue: <Divider sx={{mt: 1, mb: 1}}/>
             &nbsp;1) Photo Id Verification<br />
             &nbsp;2) Property Owner Verification<br />
             &nbsp;3) AML compliance (Paypal / Stripe)<br />
